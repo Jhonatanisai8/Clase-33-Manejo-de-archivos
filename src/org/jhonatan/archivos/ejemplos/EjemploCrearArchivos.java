bc8889ea811b07d7ccb0c9abf2fc18bba19634af
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-01 15:53:42 -0500

    clase main: en esta clase llamamos a los metodos que crean el archivo

---
+++ src/org/jhonatan/archivos/ejemplos/EjemploCrearArchivos.java
@@ -0,0 +1,16 @@
+package org.jhonatan.archivos.ejemplos;
+
+import org.jhonatan.archivos.ejemplos.servicio.ArchivoServicio;
+
+public class EjemploCrearArchivos {
+    public static void main(String[] args) {
+        System.out.println("CREANDO ARCHIVOS CON JAVA");
+
+        // le pamos la ruta
+        String nombreArchivo = "C:\\Users\\User\\Desktop\\miPrimerArchivo.txt";
+
+        // instancia de ArchivoServicio
+        ArchivoServicio service = new ArchivoServicio();
+        service.crearArchivo(nombreArchivo);
+    }
+}
