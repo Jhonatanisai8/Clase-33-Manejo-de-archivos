d4f9ccae7784ba89c1df15e60e135f68d9f5a7ff
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-01 16:07:50 -0500

    otro forma de crear y escribir informacion sobreun archivo

--- src/org/jhonatan/archivos/ejemplos/EjemploCrearArchivos.java
+++ src/org/jhonatan/archivos/ejemplos/EjemploCrearArchivos.java
@@ -11,6 +11,6 @@ public class EjemploCrearArchivos {
 
         // instancia de ArchivoServicio
         ArchivoServicio service = new ArchivoServicio();
-        service.crearArchivo(nombreArchivo);
+        service.crearArchivo02(nombreArchivo);
     }
 }
--- src/org/jhonatan/archivos/ejemplos/servicio/ArchivoServicio.java
+++ src/org/jhonatan/archivos/ejemplos/servicio/ArchivoServicio.java
@@ -1,5 +1,6 @@
 package org.jhonatan.archivos.ejemplos.servicio;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
@@ -15,6 +16,26 @@ public class ArchivoServicio {
                     .append("Todo Bien? yo aca escribiendo un archivo....\n")
                     .append("Hasta luego Lucas!...\n");
             escritor.close();
+            System.out.println("El archivo se ah creado con exito...");
+
+        } catch (Exception e) {
+            System.out.println("Error al crear archivo: " + e.getMessage());
+        }
+    }
+
+     public void crearArchivo02(String nombre) {
+        File archivo = new File(nombre);
+
+        try {
+            FileWriter escritor = new FileWriter(archivo, true);// true para conservar lo escrito si tiene contenido agrega nuevo contenido
+
+            BufferedWriter buffer = new BufferedWriter(escritor);
+
+            // escribimos
+            buffer.append("Hola que tal Amigos!.\n")
+                    .append("Todo Bien? yo aca escribiendo un archivo....\n")
+                    .append("Hasta luego Lucas!...\n");
+            buffer.close();
             System.out.println("El archivo se ah creado con exito...");
 
         } catch (Exception e) {
